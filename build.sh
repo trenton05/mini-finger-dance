93cf33b646f386f4c64b63ce631824ec0e7faa82
Trenton <trenton@PKI-00552-MacBook-Pro.local>
2023-12-18 14:24:36 -0600

    f

--- build.sh
+++ build.sh
@@ -19,17 +19,21 @@ function processApk() {
     apksigner sign --ks platform.keystore --ks-pass pass:schmuck --ks-key-alias platform $NAME.apk
 }
 
-if [ ! -f "./original/mfd.xapk" ]; then
-    echo "./original/mfd.xapk not found, download it here https://m.apkpure.com/mini-finger-dance/com.yxjh.antigravitydance and place in ./original/mfd.xapk"
-    exit 1
-fi
-
 BUILD_TOOLS=`ls -d $ANDROID_HOME/build-tools/* | sort -r | head -1`
 if [ ! -f "$BUILD_TOOLS/apksigner" ]; then
     echo "Build tools apksigner not found in ANDROID_HOME/build-tools: $ANDROID_HOME"
     exit 1
 fi
 export PATH=$PATH:$BUILD_TOOLS
+
+mkdir -p ./original
+if [ ! -f "./original/mfd.xapk" ]; then
+    curl -o ./original/mfd.xapk 'https://d-03.winudf.com/b/XAPK/Y29tLnl4amguYW50aWdyYXZpdHlkYW5jZV8xMTc1Xzg0Njg3ZDVm?_fn=TWluaSBGaW5nZXIgRGFuY2VfMS4xLjdfQXBrcHVyZS54YXBr&_p=Y29tLnl4amguYW50aWdyYXZpdHlkYW5jZQ%3D%3D&download_id=1966400415650400&is_hot=false&k=eb6306d960f9442baa647c7057c36bc46581fb02'
+    if [ $? != 0 ]; then
+        echo "./original/mfd.xapk not found, download it here https://m.apkpure.com/mini-finger-dance/com.yxjh.antigravitydance and place in ./original/mfd.xapk"
+        exit 1
+    fi
+fi
 
 echo "Extracting XAPK"
 cd original
@@ -40,3 +44,5 @@ cd ..
 processApk config.arm64_v8a
 processApk base_assets
 processApk com.yxjh.antigravitydance
+
+./install.sh
