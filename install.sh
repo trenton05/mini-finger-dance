f770fbf24d28d4705f666dca44c1ff80dcc8b129
Trenton <trenton@PKI-00552-MacBook-Pro.local>
2023-12-18 13:23:09 -0600

    f

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 function processApk() {
+    echo "Extracting $NAME"
     export NAME=$1
-    rm -fR $NAME
+    rm -fR $NAME $NAME*.apk
     mkdir $NAME
     unzip -d $NAME ./original/$NAME.apk
 
+    echo "Processing $NAME"
     node $NAME.js
 
+    echo "Zipping $NAME"
     cd $NAME
     zip -r ../$NAME-unaligned.apk *
     cd ..
     zipalign -p 4 $NAME-unaligned.apk $NAME.apk
-    apksigner sign --ks platform.keystore --ks-pass schmuck $NAME.apk --ks-key-alias platform
+    echo "Signing $NAME"
+    apksigner sign --ks platform.keystore --ks-pass pass:schmuck --ks-key-alias platform $NAME.apk
+    echo "Installing $NAME"
     adb install $NAME.apk
 }
 
@@ -22,18 +27,18 @@ if [ ! -f "./original/mfd.xapk" ]; then
 fi
 
 BUILD_TOOLS=`ls -d $ANDROID_HOME/build-tools/* | sort -r | head -1`
-export BUILD_TOOLS_PATH=$ANDROID_HOME/build-tools/$BUILD_TOOLS
-if [ ! -f "$BUILD_TOOLS_PATH/apksigner" ]; then
+if [ ! -f "$BUILD_TOOLS/apksigner" ]; then
     echo "Build tools apksigner not found in ANDROID_HOME/build-tools: $ANDROID_HOME"
     exit 1
 fi
-export PATH=$PATH:$BUILD_TOOLS_PATH
+export PATH=$PATH:$BUILD_TOOLS
 
+echo "Extracting XAPK"
 cd original
-rm -f *.apk
+rm -f *.apk *.png *.json
 unzip mfd.xapk
 cd ..
 
 processApk com.yxjh.antigravitydance
-processApk base_assets
 processApk config.arm64_v8a
+processApk base_assets
